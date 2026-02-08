51d9d21277297458876cd410101109830313784e
Robin Schmöcker <55958538+codebro634@users.noreply.github.com>
2026-02-09 00:29:54 +0100

    Fix diagonal left values in JoinFive.cpp

--- src/Games/MDPs/JoinFive.cpp
+++ src/Games/MDPs/JoinFive.cpp
@@ -162,7 +162,7 @@ Model::Model(bool joint, bool exit_on_out_of_bounds, bool decoupled_action_space
         {{0, -1}, {1, 0, 0}},
 
         {{1, 1}, {2, 0, 0}}, //diagonal left
-        {{-1, -1}, {2, -1, -1}},
+        {{-1, -1}, {2, 1, 1}},
 
         {{1, -1}, {3, -1, 0}}, //diagonal right
         {{-1, 1}, {3, 0, -1}},
